4a6c4cc1f0ed2bea1cdb67bdbad09e0ecaeb8f0e
theMegaEXP <theMegaEXP74@gmail.com>
2024-05-31 16:44:04 -0700

    can connect to addSymptomsPage from symptomsPage

--- gui/pages/symptomsPage.py
+++ gui/pages/symptomsPage.py
@@ -8,3 +8,8 @@ class SymptomsPage:
         self.widget = QWidget()
         self.ui = Ui_symptomsPage()
         self.ui.setupUi(self.widget)
+
+        self.form_setup()
+
+    def form_setup(self):
+        self.ui.returnBtn.clicked.connect(lambda: self.mw.page_connect_add_symptom())
