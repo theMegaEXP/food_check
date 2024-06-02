3b2a09e67d1e18a2c72bbe4cb4cb5db118d7943c
theMegaEXP <theMegaEXP74@gmail.com>
2024-06-02 00:02:35 -0700

    setup context menu for symptomListing

--- gui/widgets/symptomListing.py
+++ gui/widgets/symptomListing.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QMenu
+from PyQt5.QtCore import Qt
 
 from gui.designer.Ui_symptomListing import Ui_symptomListing
 
@@ -10,9 +11,25 @@ class SymptomListing:
         self.ui.setupUi(self.widget)
 
         self.alter_fields()
+        self.context_menu_setup()
 
     def alter_fields(self):
         self.ui.symptomLabel.setText(self.kwargs['symptom'].title())
         self.ui.severityLabel.setText(self.kwargs['severity'])
         self.ui.dateLabel.setText(self.kwargs['date'])
         self.ui.timeLabel.setText(self.kwargs['time'])
+
+    def context_menu_setup(self):
+        self.widget.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.widget.customContextMenuRequested.connect(self.show_context_menu)
+
+    def show_context_menu(self, pos):
+        context_menu = QMenu(self)
+        update_action = context_menu.addAction("Update")
+        delete_action = context_menu.addAction("Delete")
+        action = context_menu.exec_(self.widget.mapToGlobal(pos))
+
+        if action == update_action:
+            print("update")
+        elif action == delete_action:
+            print("delete")
