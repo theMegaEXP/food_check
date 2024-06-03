598a2e15c02fe028b2627b1229879a2b35b1d231
theMegaEXP <theMegaEXP74@gmail.com>
2024-06-03 12:26:24 -0700

    fix issues in Foods.store()

--- models/foods.py
+++ models/foods.py
@@ -20,10 +20,10 @@ class Foods:
             # Insert into products table
             if data['barcode'] and data['product']:
                 if not DB.Query.value_exists('products', 'product', data['product']) and not DB.Query.value_exists('products', 'barcode', data['barcode']):
-                    DB.Query.insert_into('proudcts', ['proudct', 'barcode'], [data['product'], data['barcode']])
+                    DB.Query.insert_into('products', ['product', 'barcode'], [data['product'], data['barcode']])
             elif data['barcode']:
                 if not DB.Query.value_exists('products', 'product', data['product']):
-                    DB.Query.insert_into('proudcts', ['proudct'], [data['barcode']])
+                    DB.Query.insert_into('products', ['product'], [data['barcode']])
             elif data['products']:
                 if not DB.Query.value_exists('barcode', 'products', data['product']):
                     DB.Query.insert_into('products', ['product'], [data['barcode']])
@@ -35,13 +35,14 @@ class Foods:
         for ingredient in data['ingredients']:
             
             # Insert into ingredients table
-            if not DB.Query.value_exists('ingredients', 'ingredient', data['ingredients']):
+            if not DB.Query.value_exists('ingredients', 'ingredient', ingredient):
                 DB.Query.insert_into('ingredients', ['ingredient'], [ingredient])
 
             # Insert into product_ingredients table
             if product_provided:
                 product_id = DB.Query.fetch_id('products', 'barcode', data['barcode']) or DB.Query.fetch_id('products', 'product', data['product'])
                 ingredient_id = DB.Query.fetch_id('ingredients', 'ingredient', ingredient)
+                print(product_id, ingredient_id)
                 DB.Query.insert_into('product_ingredients', ['product_id', 'ingredient_id'], [product_id, ingredient_id])
 
             # Insert into ingredient_times table
