f9763a45b24308e7271517f964c74d4080bdc73c
theMegaEXP <theMegaEXP74@gmail.com>
2024-06-04 21:02:53 -0700

    rename ingredient_times to product_ingredient_times in Foods.store()

--- models/foods.py
+++ models/foods.py
@@ -41,12 +41,12 @@ class Foods:
             if not DB.Query.value_exists('ingredients', 'ingredient', ingredient):
                 DB.Query.insert_into('ingredients', ['ingredient'], [ingredient])
 
-            # Insert into ingredient_times table
+            # Insert into product_ingredient_times table
             ingredient_id = DB.Query.fetch_id('ingredients', 'ingredient', ingredient)
             if product_provided:
-                DB.Query.insert_into('ingredient_times', ['ingredient_id', 'product_id', 'date', 'time', 'datetime'], [ingredient_id, product_id, data['date'], data['time'], Time.format_datetime(data['date'], data['time'])])
+                DB.Query.insert_into('product_ingredient_times', ['ingredient_id', 'product_id', 'date', 'time', 'datetime'], [ingredient_id, product_id, data['date'], data['time'], Time.format_datetime(data['date'], data['time'])])
             else:
-                DB.Query.insert_into('ingredient_times', ['ingredient_id', 'date', 'time', 'datetime'], [ingredient_id, data['date'], data['time'], Time.format_datetime(data['date'], data['time'])])
+                DB.Query.insert_into('product_ingredient_times', ['ingredient_id', 'date', 'time', 'datetime'], [ingredient_id, data['date'], data['time'], Time.format_datetime(data['date'], data['time'])])
 
     def update():
         pass
