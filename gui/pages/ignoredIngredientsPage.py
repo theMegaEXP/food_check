3bfcc228b714f531600837fceda00c5c7abed526
theMegaEXP <theMegaEXP74@gmail.com>
2024-06-07 20:52:50 -0700

    can add ignored ingredients to ui

--- gui/pages/ignoredIngredientsPage.py
+++ gui/pages/ignoredIngredientsPage.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QLineEdit
 
 from gui.designer.Ui_ignoredIngredientsPage import Ui_ignoredIngredientsPage
+from gui.widgets.itemInput import ItemInput
+from models.ignoredIngredients import IgnoredIngredients
 
 class IgnoredIngredientsPage():
     def __init__(self, main_window):
@@ -8,3 +10,32 @@ class IgnoredIngredientsPage():
         self.widget = QWidget()
         self.ui = Ui_ignoredIngredientsPage()
         self.ui.setupUi(self.widget)
+
+        self.form_setup()
+        self.widget_setup
+
+    def form_setup(self):
+        self.ui.returnBtn.clicked.connect(lambda: self.save_items())
+        self.ui.addBtn.clicked.connect(lambda: self.add_item_input())
+
+    def widget_setup(self):
+        ingredients = IgnoredIngredients.fetch()
+        for ingredient in ingredients:
+            item_input = ItemInput(self, ingredient[0])
+            self.ui.ingredientsLayout.addWidget(item_input.widget)
+
+    def add_item_input(self):
+        item_input = ItemInput(self)
+        self.ui.ingredientsLayout.addWidget(item_input.widget)
+
+    def save_items(self):
+        IgnoredIngredients.reset()
+
+        for i in range(self.ui.ingredientsLayout.count()):
+            widget = self.ui.ingredientsLayout.itemAt(i).widget()
+            if isinstance(widget, QWidget):
+                input_text = widget.findChild(QLineEdit, "input").text()
+                if input_text != '':
+                    IgnoredIngredients.store(input_text)
+
+        self.mw.page_connect_home()
