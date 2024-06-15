d95390014e49f8765b607f52bb348e9e75c32f98
theMegaEXP <theMegaEXP74@gmail.com>
2024-06-14 19:43:33 -0700

    add back button to CalculatePage

--- gui/pages/calculatePage.py
+++ gui/pages/calculatePage.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget
 
 from gui.designer.Ui_calculatePage import Ui_calculatePage
+from gui.widgets.backButton import BackButton
 
 class CalculatePage:
     def __init__(self, main_window):
@@ -8,3 +9,8 @@ class CalculatePage:
         self.widget = QWidget()
         self.ui = Ui_calculatePage()
         self.ui.setupUi(self.widget)
+
+        self.widget_setup()
+
+    def widget_setup(self):
+        self.ui.mainLayout.insertWidget(0, BackButton(self.mw))
